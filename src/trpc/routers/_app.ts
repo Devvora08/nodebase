5ce2763d2ff1f2cafd923f235943bcdda4cb5e02
devvora08 <devvora08@gmail.com>
2025-10-28 18:41:28 +0530

    07: ai-providers

--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -2,12 +2,24 @@ import { z } from 'zod';
 import { createTRPCRouter, protectedProcedure } from '../init';
 import prisma from '@/lib/db';
 import { inngest } from '@/inngest/client';
+import { google } from '@ai-sdk/google';
+import { generateText } from 'ai';
+
 export const appRouter = createTRPCRouter({
+  testAi: protectedProcedure.mutation(async () => {
+    await inngest.send({
+      name: "execute/ai",
+    })
+
+    return {success: true, message: "Job is queued"};
+  }),
+
   getWorkflows: protectedProcedure
-    .query(({ctx}) => {
+    .query(({ ctx }) => {
       return prisma.workflow.findMany();
   }),
-  createWorkflow: protectedProcedure.mutation(async() => {
+
+  createWorkflow: protectedProcedure.mutation(async () => {
     await inngest.send({
       name: "test/hello.world",
       data: {
