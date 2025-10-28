3847af0ab2226ab4b07adc0dba60eacdbf9cd191
devvora08 <devvora08@gmail.com>
2025-10-28 17:22:56 +0530

    06: background jobs

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,16 +1,30 @@
+"use client"
 import { Button } from "@/components/ui/button"
 import { authClient } from "@/lib/auth-client"
 import { requireAuth } from "@/lib/auth-utils"
 import { caller } from "@/trpc/server"
 import { LogoutBtn } from "./logout"
+import { useTRPC } from "@/trpc/client"
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query"
 
-const Page = async () => {
-  await requireAuth()
-  const data = await caller.getUsers();
+const Page = () => {
+  const trpc = useTRPC()
+  const queryClient = useQueryClient()
+  const {data} = useQuery(trpc.getWorkflows.queryOptions())
+  const create = useMutation(trpc.createWorkflow.mutationOptions({
+    onSuccess: () => {
+      queryClient.invalidateQueries(trpc.getWorkflows.queryOptions())
+    }
+  }))
   return (
     <div>
      protected server component
      {JSON.stringify(data)}
+     <div>
+        <Button disabled={create.isPending} onClick={() => create.mutate()}>
+          Create a workflow
+        </Button>
+     </div>
      <LogoutBtn/>
     </div>
   )
--- src/trpc/routers/_app.ts
+++ src/trpc/routers/_app.ts
@@ -1,15 +1,26 @@
 import { z } from 'zod';
-import { baseProcedure, createTRPCRouter, protectedProcedure } from '../init';
+import { createTRPCRouter, protectedProcedure } from '../init';
 import prisma from '@/lib/db';
+import { inngest } from '@/inngest/client';
 export const appRouter = createTRPCRouter({
-  getUsers: protectedProcedure
+  getWorkflows: protectedProcedure
     .query(({ctx}) => {
-      return prisma.user.findMany({
-        where: {
-          id: ctx.auth.user.id
-        }
-      });
-    }),
+      return prisma.workflow.findMany();
+  }),
+  createWorkflow: protectedProcedure.mutation(async() => {
+    await inngest.send({
+      name: "test/hello.world",
+      data: {
+        email: "dev@mail.com"
+      }
+    });
+
+    return prisma.workflow.create({
+      data: {
+        name: "test-workflow"
+      }
+    })
+  }),
 });
 // export type definition of API
 export type AppRouter = typeof appRouter;
