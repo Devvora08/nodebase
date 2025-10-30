c68aeb4c2c647d9c1d3f9a853706ac512c102691
devvora08 <devvora08@gmail.com>
2025-10-30 09:23:11 -0300

    10: polar-payments

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,6 +1,8 @@
 import { betterAuth } from "better-auth";
 import { prismaAdapter } from "better-auth/adapters/prisma";
 import prisma from "./db";
+import { polar, checkout, portal, usage, webhooks } from "@polar-sh/better-auth";
+import { polarClient } from "./polar";
 
 export const auth = betterAuth({
     database: prismaAdapter(prisma, {
@@ -9,5 +11,24 @@ export const auth = betterAuth({
     emailAndPassword: {
         enabled: true,
         autoSignIn: true,
-    }
+    },
+    plugins: [
+        polar({
+            client: polarClient,
+            createCustomerOnSignUp: true,
+            use: [
+                checkout({
+                    products:[
+                        {
+                            productId: "714e84fb-9498-45ea-8715-14e5989eef85",
+                            slug: "pro",
+                        }
+                    ],
+                    successUrl: process.env.POLAR_SUCCESS_URL,
+                    authenticatedUsersOnly: true,
+                }),
+                portal(),
+            ]
+        })
+    ]
 });
