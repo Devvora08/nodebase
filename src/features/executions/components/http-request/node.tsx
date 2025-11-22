be0796f0ef30d5ead9ca502095f2f839e39e0ede
devvora08 <devvora08@gmail.com>
2025-11-22 10:29:15 +0530

    21: node-real-time

--- src/features/executions/components/http-request/node.tsx
+++ src/features/executions/components/http-request/node.tsx
@@ -5,6 +5,9 @@ import { BaseExecutionNode } from "../base-execution-node"
 import { memo, useState } from "react";
 import { GlobeIcon } from "lucide-react";
 import { HttpRequestFormValues, HttpRequestDialog } from "./dialog";
+import { useNodeStatus } from "../../hooks/use-node-status";
+import { HTTP_REQUEST_CHANNEL_NAME } from "@/inngest/channels/http-request";
+import { fetchHttpRequestRealtimeToken } from "./actions";
 
 type HttpRequestNodeData = {
     variableName?: string;
@@ -24,7 +27,12 @@ export const HttpRequestNode = memo((props: NodeProps<HttpRequestNodeType>) => {
     ? `${nodeData.method || "GET"}: ${nodeData.endpoint}`
     : "Not configured";
 
-    const nodeStatus = "initial";
+    const nodeStatus = useNodeStatus({
+        nodeId: props.id,
+        channel: HTTP_REQUEST_CHANNEL_NAME,
+        topic: "status",
+        refreshToken: fetchHttpRequestRealtimeToken
+    })
 
     const handleSubmit = (values: HttpRequestFormValues) => {
         setNodes(nodes => nodes.map(node => {
--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -4,11 +4,19 @@ import prisma from "@/lib/db";
 import { topologicalSort } from "./utils";
 import { NodeType } from "@/generated/prisma";
 import { getExecutor } from "@/features/executions/lib/executer-registry";
+import { httpRequestChannel } from "./channels/http-request";
+import { manualTriggerChannel } from "./channels/manual-trigger";
 
 export const executeWorkflow = inngest.createFunction(
-  { id: "execute-worflow" },
-  { event: "workflows/execute-workflow" },
-  async({event, step}) => {
+  { id: "execute-worflow", retries: 0 },
+  {
+     event: "workflows/execute-workflow",
+     channels: [
+      httpRequestChannel(),
+      manualTriggerChannel(),
+     ],
+  },
+  async({event, step, publish}) => {
     const workflowId = event.data.workflowId;
 
     if(!workflowId) {
@@ -37,7 +45,8 @@ export const executeWorkflow = inngest.createFunction(
         data: node.data as Record<string, unknown>,
         nodeId: node.id,
         context,
-        step
+        step,
+        publish
       })
     }
 
