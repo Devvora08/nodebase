5cb860fa76792765671e845e89d5656f5caf74f0
devvora08 <devvora08@gmail.com>
2025-11-29 11:38:33 +0530

    29: github-google-auth

--- src/features/auth/components/register-form.tsx
+++ src/features/auth/components/register-form.tsx
@@ -54,6 +54,32 @@ export function RegisterForm() {
         )
     }
 
+    const signInGithub = async() => {
+        await authClient.signIn.social({
+            provider: "github",
+        }, {
+            onSuccess: () => {
+                router.push("/");
+            },
+            onError: () => {
+                toast.error("Something went wrong")
+            }
+        })
+    }
+
+    const signInGoogle = async() => {
+        await authClient.signIn.social({
+            provider: "google",
+        }, {
+            onSuccess: () => {
+                router.push("/");
+            },
+            onError: () => {
+                toast.error("Something went wrong")
+            }
+        })
+    }
+
     const isPending = form.formState.isSubmitting;
 
     return (
@@ -72,8 +98,8 @@ export function RegisterForm() {
                         <form onSubmit={form.handleSubmit(onSubmit)}>
                             <div className="grid gap-6">
                                 <div className="flex flex-col gap-4">
-                                    <Button variant={'outline'} className="w-full" type="button" disabled={isPending}><Image alt="github" src={'/github.svg'} width={20} height={20}/> Continue with GitHub</Button>
-                                    <Button variant={'outline'} className="w-full" type="button" disabled={isPending}><Image alt="github" src={'/google.svg'} width={20} height={20}/> Continue with Google</Button>
+                                    <Button variant={'outline'} className="w-full" type="button" disabled={isPending} onClick={signInGithub}><Image alt="github" src={'/github.svg'} width={20} height={20}/> Continue with GitHub</Button>
+                                    <Button variant={'outline'} className="w-full" type="button" disabled={isPending} onClick={signInGoogle}><Image alt="github" src={'/google.svg'} width={20} height={20}/> Continue with Google</Button>
                                 </div>
                                 <div className="grid gap-6">
                                     <FormField control={form.control} name="email" render={({ field }) => (
