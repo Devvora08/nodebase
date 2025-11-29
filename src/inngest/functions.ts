8c9fddd61be23eb1921587b4bdacca962cbfccc0
devvora08 <devvora08@gmail.com>
2025-11-29 11:43:00 +0530

    30: deployment

--- src/inngest/functions.ts
+++ src/inngest/functions.ts
@@ -16,7 +16,7 @@ import { slackChannel } from "./channels/slack";
 
 export const executeWorkflow = inngest.createFunction(
   {
-    id: "execute-worflow", retries: 0, onFailure: async ({ event, step }) => {
+    id: "execute-worflow", retries: process.env.NODE_ENV === "production" ? 3 : 0, onFailure: async ({ event, step }) => {
       return prisma.execution.update({
         where: { inngestEventId: event.data.event.id },
         data: {
